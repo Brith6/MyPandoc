844e9402bc3f83a85840b35606d31771fce4536d
AGONGBONON Tobi Prince <tobi.agongbonon@epitech.eu>
2025-04-30 14:34:56 +0100

    fix: Modifying some types

--- src/Document.hs
+++ src/Document.hs
@@ -13,46 +13,51 @@ data Header = Header {
     _headtitle::Maybe String,
     author::Maybe String,
     date::Maybe String
-}
+} deriving (Show)
 
 data Paragraph = Paragraph {
     a_paragraph :: [Text]
-}
+} deriving (Show)
 
-data Codeblock = Codeblock {
-    a_codeblock :: [Paragraph]
-}
+-- data Codeblock = Codeblock {
+--     a_codeblock :: Mayb
+-- } deriving (Show)
 
 data Option = Option {
-    a_list :: [Paragraph]
-}
+    a_list :: [BodyElem]
+}deriving (Show)
 
 data Text = Text {
     encapsulator :: Maybe String,
+    bold :: Bool,
+    italic:: Bool,
     value :: Maybe String,
     url :: Maybe String
-}
+}deriving (Show)
 
 data SectionElem = SectionElem {
     _section :: Maybe Section,
     _secpara :: Maybe Paragraph,
-    _seccode :: Maybe Codeblock,
+    _secblockcode :: Maybe String,
     _seclist :: Maybe Option
-}
+}deriving (Show)
+
 data Section = Section {
     _sectitle :: String,
     content :: [SectionElem]
-}
+}deriving (Show)
 
 data BodyElem = BodyElem {
     _bdysection :: Maybe Section,
     _bdypara :: Maybe Paragraph,
     _bdylist :: Maybe Option,
-    _bdycode :: Maybe Codeblock
-} 
+    _bdycodeblock :: Maybe String
+} deriving (Show)
 
 data Doc = Doc {
     header::Header,
     body::[BodyElem]
-}
+} deriving (Show)
 
+defaultDoc :: Doc
+defaultDoc = Doc (Header Nothing Nothing Nothing) []
